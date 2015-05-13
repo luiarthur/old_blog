fd81617bc94e9474c7b3a83465be647342a06c05
luiarthur <luiarthur@gmail.com>
2015-05-13 10:39:27 -0600

    tes

--- js/callInlineDisqussions.js
+++ js/callInlineDisqussions.js
@@ -2,3 +2,14 @@ disqus_shortname = 'luiarthurgithubio';
 jQuery(document).ready(function() {
   jQuery("p").inlineDisqussions();
 });
+
+/* Default settings: Change these and put in html
+ jQuery("p").inlineDisqussions({
+  identifier: 'disqussion',
+  displayCount: true,
+  highlighted: false,
+  position: 'right',
+  background: 'white',
+  maxWidth: 9999
+});
+*/
