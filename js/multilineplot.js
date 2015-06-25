95658bc86795559a12d2e2ec9f20629f536c1e1e
luiarthur <luiarthur@gmail.com>
2015-06-25 14:12:45 -0600

    yes

--- js/multilineplot.js
+++ js/multilineplot.js
@@ -1,8 +1,8 @@
 function draw(filename,xcol,xlo,xhi,ylo,yhi) {
   /*Page Setup:*/
   var margin = {top: 20, right: 80, bottom: 30, left: 50},
-      width = 1000 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+      width = (1000 - margin.left - margin.right) * .5,
+      height = (500 - margin.top - margin.bottom) * .5;
   var svg = d3.select("body").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
